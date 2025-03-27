2bc80de24241b4355c4c3e4e33545f9549a6d815
KarnaukhRoman <karnaukhrv@ukr.net>
2025-03-27 22:54:03 +0100

    Compilation with sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
-import './index.scss'
+// import './index.scss'
+import '../css/index.css';
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
